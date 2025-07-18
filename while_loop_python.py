7ec10479937b81938182b49aa414e01c6d9b3b50
rgdevengineer <ritwickghosh.cse@gmail.com>
2025-07-18 23:15:50 +0530

    Program Files from Geeks For Geeks Submitted

--- while_loop_python.py
+++ while_loop_python.py
@@ -1,11 +1,9 @@
 
 '''
 While loop in Python
-Congratulations! You are one more step close to Python Programming World. You are now familiar with if-elif-else in Python, and for loop in Python.
 
-While loop in Python is same as like in CPP and Java, but, here you have to use ':' to end while statement (used to end any statement). While loop is used to iterate same as for loop, except that in while loop you can customize jump of steps in coupling with variable used to loop, after every iteration, unlike in for loop (you cannot customize jump according to the variable you are using to loop through).
-
-Let's get it more clearly through this question. Given a number x, the task is to print the numbers from x to 0 in decreasing order in a single line.
+Let's get it more clearly through this question. Given a number x, 
+the task is to print the numbers from x to 0 in decreasing order in a single line.
 
 Example:
 
